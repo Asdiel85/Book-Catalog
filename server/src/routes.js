bab9f052ef8840f08cfa1cf88a7ef7d56cbd4149
Martin Hristov <mghristov85@gmail.com>
2024-02-19 14:59:03 +0200

    added user registration and login

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const authController = require('./controlers/authController')
 const bookController = require('./controlers/bookControler')
 
+router.use('/auth', authController)
 router.use('/books', bookController)
 
 module.exports = router
